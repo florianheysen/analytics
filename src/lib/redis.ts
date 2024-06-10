738d9fc2b077a88d2166e1c75f9b2cb31cb2bee2
Florian Heysen <heysen.florian@gmail.com>
2024-06-10 16:53:30 +0200

    db change

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,6 +1,6 @@
 import { Redis } from "@upstash/redis";
 
 export const redis = new Redis({
-  url: "https://good-kangaroo-54679.upstash.io",
+  url: "https://funny-boxer-54682.upstash.io",
   token: process.env.REDIS_KEY!,
 });
